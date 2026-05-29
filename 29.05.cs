c09ced0fc15478750b46e8a7d47f49079fb2997d
Dawid Pietrasz <dawidpietrasz@proton.me>
2026-05-29 10:56:48 +0200

    Create 29.05.cs

---
+++ 29.05.cs
@@ -0,0 +1,32 @@
+    class Student{
+        string imie;
+        string nazwisko;
+        double[] oceny;
+        dodajOcene(double ocena){
+            oceny.Add(ocena);
+        }
+        obliczSrednia(){
+            int liczby;
+            for (int i = 0; i < oceny.length; i++){
+                liczby=liczby+oceny[i];
+                return liczby;
+            }
+            liczby = liczby/oceny.length;
+            return liczby;
+        }
+    }
+    class Product{
+        string nazwa;
+        int ilosc;
+        double cenaJednostkowa;
+        dodajProdukty(int ilosc){
+            this.ilosc=this.ilosc+ilosc;
+            return this.ilosc;
+        }
+        usunProdukty(int ilosc){
+            if (ilosc<this.ilosc)
+                this.ilosc=this.ilosc-ilosc;
+            else
+                return -1;
+        }
+    }
